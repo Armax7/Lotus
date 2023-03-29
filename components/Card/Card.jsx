31ab5e3c87ab2da21006a6259d3b755ea4acc17f
Armando Isaac Garcia <armando7jx@gmail.com>
2023-03-29 13:57:27 -0600

    Created component Card

--- components/Card/Card.jsx
+++ components/Card/Card.jsx
@@ -1,28 +1,44 @@
-// import { WrapItem,CardBody,Card, Heading,Text, Stack, Divider, CardFooter, Button, ButtonGroup, SimpleGrid, VStack, Wrap} from "@chakra-ui/react";
-
 import * as Chakra from "@chakra-ui/react";
 
-function Cards({ artwork, ...props }) {
-  const { name, price, image } = artwork;
+function Card({
+  artwork,
+  spacing: spacingProp = 4,
+  className: classNameProp = null,
+  ...props
+}) {
+  const {
+    id,
+    name,
+    image,
+    size,
+    price,
+    rating,
+    stock,
+    available,
+    technique_id,
+    category_id,
+    support_id,
+    author_id,
+  } = artwork;
 
   return (
-    <div>
-      hola que tal esto es una targeta con {name} {price}{" "}
-    </div>
+    <Chakra.VStack className={classNameProp} spacing={spacingProp} {...props}>
+      <Chakra.WrapItem>
+        <Chakra.Stack mt="6" spacing="3">
+          <Chakra.Card maxW="sm">
+            <Chakra.CardBody>
+              <img src={image} alt={name} width={300} height={300} />
+
+              <Chakra.Text color="blue.600" fontSize="2xl">
+                {price}
+              </Chakra.Text>
+              <Chakra.Heading size="md">{name}</Chakra.Heading>
+            </Chakra.CardBody>
+          </Chakra.Card>
+        </Chakra.Stack>
+      </Chakra.WrapItem>
+    </Chakra.VStack>
   );
 }
 
-export default Cards;
-
-/*  <Stack mt='6' spacing='3'>
-      <Heading size='md'>Living room Sofa</Heading>
-      <Text>
-        This sofa is perfect for modern tropical spaces, baroque inspired
-        spaces, earthy toned spaces and for people who love a chic design with a
-        sprinkle of vintage design.
-      </Text>
-      <Text color='blue.600' fontSize='2xl'>
-        $450
-      </Text>
-    </Stack>
-  </CardBody> */
+export default Card;
