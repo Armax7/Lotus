fd596f1c27fc80fd75bf6ccb152c2e53cb108cca
unknown <jorgemathez14@gmail.com>
2023-03-29 22:27:29 -0300

    created navbar

---
+++ components/NavBar/NavBar.jsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import * as Chakra from  "@chakra-ui/react"
+
+function NavBar({classname, ...props}) {
+    const router = useRouter();
+
+  return (
+<Chakra.Flex as="nav" className={classname} {...props}> 
+<Link href="/Search" >
+    <Chakra.Button bgColor="blackAlpha.800" color="red.600">Search</Chakra.Button>
+    </Link>
+    </Chakra.Flex>
+   
+  )
+}
+
+export default NavBar;
