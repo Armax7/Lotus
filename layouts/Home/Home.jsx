28704b0552d2421b0d63e398c081982e650b88ce
Marina <95050756+Marindala@users.noreply.github.com>
2023-03-31 20:03:52 -0300

    Layouts Home

---
+++ layouts/Home/Home.jsx
@@ -0,0 +1,35 @@
+import * as Components from "../../components";
+import styles from "../../styles/Home.module.css";
+import * as Chakra from "@chakra-ui/react";
+
+function Home({ artworks }) {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <img src="/Lotus_Logo.jpeg" alt="Loto" />
+
+        <p className={styles.description}>
+          <code>
+            SOMOS LOTUS Te ayudamos a convertir tu casa, departamento, oficina,
+            dormitorio o el lugar que quieras, en tu espacio favorito.
+          </code>
+        </p>
+        <Chakra.Box
+          as="h1"
+          textAlign="center"
+          fontSize="3xl"
+          fontWeight="bold"
+          color="black"
+          textShadow="2px 2px 4px rgba(0, 0, 0, 0.3)"
+          paddingTop="15px"
+        >
+          ✨CUADROS DESTACADOS DE NUESTRA SELECCIÓN✨
+        </Chakra.Box>
+
+        <Components.CardContainer cards={artworks} columns={3} />
+      </main>
+    </div>
+  );
+}
+
+export default Home;
--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import * as Chakra from "@chakra-ui/react";
 import * as ReactQuery from "@tanstack/react-query";
 import * as QueryKeys from "../helpers/page_helpers/Home_helpers/query_keys";
 import * as QueryFn from "../helpers/page_helpers/Home_helpers/query_fn";
+import * as Layouts from "../layouts";
 
 export default function Home() {
   const queryClient = ReactQuery.useQueryClient();
@@ -28,33 +29,7 @@ export default function Home() {
     return <h1>Error: {artwork_error}</h1>;
   }
 
-  return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <img src="/Lotus_Logo.jpeg" alt="Loto" />
-
-        <p className={styles.description}>
-          <code>
-            SOMOS LOTUS Te ayudamos a convertir tu casa, departamento, oficina,
-            dormitorio o el lugar que quieras, en tu espacio favorito.
-          </code>
-        </p>
-        <Chakra.Box
-          as="h1"
-          textAlign="center"
-          fontSize="3xl"
-          fontWeight="bold"
-          color="black"
-          textShadow="2px 2px 4px rgba(0, 0, 0, 0.3)"
-          paddingTop="15px"
-        >
-          ✨CUADROS DESTACADOS DE NUESTRA SELECCIÓN✨
-        </Chakra.Box>
-
-        <Components.CardContainer cards={artworks} columns={3} />
-      </main>
-    </div>
-  );
+  return <Layouts.Home artworks={artworks} />;
 }
 
 export async function getServerSideProps() {
