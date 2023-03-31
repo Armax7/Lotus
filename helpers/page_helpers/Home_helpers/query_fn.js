d96d6daecfa95a69660a214d7bc3207d11d81565
Maxi Pogonza <pogonza00@gmail.com>
2023-03-31 11:59:12 -0300

    Creation of art-showcase routes (POST and DELETE commented)

--- helpers/page_helpers/Home_helpers/query_fn.js
+++ helpers/page_helpers/Home_helpers/query_fn.js
@@ -1,8 +1,10 @@
 import axios from "axios";
+const port = process.env.NEXT_PUBLIC_HOST || "localhost:3000"
+
 
 export async function getAllArtworksAxios() {
   const response = await axios
-    .get(`http://${process.env.NEXT_PUBLIC_HOST}/api/artworks`)
+    .get(`${port}/api/artworks`)
     .then((res) => res.data);
 
   return response;
