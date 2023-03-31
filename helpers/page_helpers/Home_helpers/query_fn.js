68a9368ecaa8e46abd79c3360ce8549c82f7de46
Armando Isaac Garcia <armando7jx@gmail.com>
2023-03-31 10:43:31 -0600

    Pulled helpers\page_helpers\Home_helpers\query_fn.js from origin development to avoid conflicts

--- helpers/page_helpers/Home_helpers/query_fn.js
+++ helpers/page_helpers/Home_helpers/query_fn.js
@@ -1,10 +1,8 @@
 import axios from "axios";
-const port = process.env.NEXT_PUBLIC_HOST || "localhost:3000"
-
 
 export async function getAllArtworksAxios() {
   const response = await axios
-    .get(`${port}/api/artworks`)
+    .get(`http://${process.env.NEXT_PUBLIC_HOST}/api/artworks`)
     .then((res) => res.data);
 
   return response;
