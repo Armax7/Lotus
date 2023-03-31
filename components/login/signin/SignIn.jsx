5e060d1ed334c04dc43871749b0e4cbe7192fde8
gekoJs <daniroa.js@gmail.com>
2023-03-31 13:48:38 -0400

    styling and adding some functionality to login

--- components/login/signin/SignIn.jsx
+++ components/login/signin/SignIn.jsx
@@ -2,69 +2,83 @@ import * as chakra from "@chakra-ui/react";
 import { FcGoogle } from "react-icons/fc";
 import { FaFacebook } from "react-icons/fa";
 import style from "../../../styles/login/signin.module.css";
+import Link from "next/link";
 
 export default function SignIn() {
+
+  function handleOnSubmit(e) {
+    e.preventDefault();
+    console.log("sign in submit press with no functionality");
+  }
+
   return (
-    <chakra.HStack w="full" h="100vh">
-      <chakra.Flex
-        w="full"
-        h="full"
-        alignItems="center"
-        justifyContent="center"
-        borderRightWidth={1}
-        display={{ base: "none", md: "flex" }}
-      >
-        <chakra.Stack w="full" maxW="md" spacing={4} p={6}>
-          <chakra.Heading fontSize="2xl">
-            Sign in to your Account
-          </chakra.Heading>
+    <form onSubmit={(e) => handleOnSubmit(e)} className={style.form}>
+      <chakra.HStack className={style.HStack}>
+        <chakra.Flex
+          w="full"
+          h="full"
+          alignItems="center"
+          justifyContent="center"
+          borderRightWidth={1}
+          display={{ base: "none", md: "flex" }}
+        >
+          <chakra.Stack w="full" maxW="md" spacing={4} p={6}>
+            <chakra.Heading fontSize="2xl">
+              Sign in to your Account
+            </chakra.Heading>
 
-          <div className={style.separator}>
-            <hr className={style.hr} />
-            <p className={style.separator_text}>With</p>
-            <hr className={style.hr} />
-          </div>
-
-          <chakra.Stack>
-            <div className={style.auth}>
-              <chakra.Button colorScheme="gray" leftIcon={<FcGoogle />}>
-                Google
-              </chakra.Button>
-              <chakra.Button colorScheme="facebook" leftIcon={<FaFacebook />}>
-                Facebook
-              </chakra.Button>
+            <div className={style.separator}>
+              <hr className={style.hr} />
+              <p className={style.separator_text}>With</p>
+              <hr className={style.hr} />
             </div>
+
+            <chakra.Stack>
+              <div className={style.auth}>
+                <chakra.Button colorScheme="gray" leftIcon={<FcGoogle />}>
+                  Google
+                </chakra.Button>
+                <chakra.Button colorScheme="facebook" leftIcon={<FaFacebook />}>
+                  Facebook
+                </chakra.Button>
+              </div>
+            </chakra.Stack>
+
+            <div className={style.separator}>
+              <hr className={style.hr} />
+              <p className={style.separator_text}>Or</p>
+              <hr className={style.hr} />
+            </div>
+
+            <chakra.FormControl id="user">
+              <chakra.FormLabel>Username Or Email</chakra.FormLabel>
+              <chakra.Input placeholder="Username / Email" />
+            </chakra.FormControl>
+
+            <chakra.FormControl id="passwordSignIn">
+              <chakra.FormLabel>Password</chakra.FormLabel>
+              <chakra.Input type="password" placeholder="******" />
+            </chakra.FormControl>
+
+            <chakra.Stack
+              spacing={4}
+              direction="row"
+              align="start"
+              justify="space-between"
+            >
+              <chakra.Checkbox>Remember me</chakra.Checkbox>
+              <chakra.Link>Forgot password</chakra.Link>
+            </chakra.Stack>
+            
+            <chakra.Button type="submit">Log in</chakra.Button>
+
+            <Link href="/ejemplo/descripcion" style={{ width: "100%" }}>
+              <chakra.Button style={{ width: "100%" }}>Sign up</chakra.Button>
+            </Link>
+
           </chakra.Stack>
-
-          <div className={style.separator}>
-            <hr className={style.hr} />
-            <p className={style.separator_text}>Or</p>
-            <hr className={style.hr} />
-          </div>
-
-          <chakra.FormControl id="user">
-            <chakra.FormLabel>Username Or Email</chakra.FormLabel>
-            <chakra.Input placeholder="Username / Email" />
-          </chakra.FormControl>
-
-          <chakra.FormControl id="password">
-            <chakra.FormLabel>Password</chakra.FormLabel>
-            <chakra.Input type="password" placeholder="******" />
-          </chakra.FormControl>
-
-          <chakra.Stack
-            spacing={4}
-            direction="row"
-            align="start"
-            justify="space-between"
-          >
-            <chakra.Checkbox>Remember me</chakra.Checkbox>
-            <chakra.Link>Forgot password</chakra.Link>
-          </chakra.Stack>
-          <chakra.Button>log in</chakra.Button>
-          <chakra.Button>Sign up</chakra.Button>
-        </chakra.Stack>
-      </chakra.Flex>
-    </chakra.HStack>
+        </chakra.Flex>
+      </chakra.HStack>
+    </form>
   );
 }
--- components/login/signup/SignUp.jsx
+++ components/login/signup/SignUp.jsx
@@ -2,50 +2,56 @@ import * as chakra from "@chakra-ui/react";
 import style from "../../../styles/login/signup.module.css";
 
 export default function SignUp() {
+
+  function handleOnSubmit(e){
+    e.preventDefault()
+    console.log("sign up submit press with no functionality")
+  }
   return (
-    <chakra.HStack w="full" h="100vh">
-      <chakra.Flex
-        w="full"
-        h="full"
-        alignItems="center"
-        justifyContent="center"
-        borderRightWidth={1}
-        display={{ base: "none", md: "flex" }}
-      >
-        <chakra.Stack w="full" maxW="md" spacing={4} p={6}>
-          <chakra.Heading fontSize="2xl">Sign up</chakra.Heading>
-          <hr className={style.hr} />
-          <div className={style.all_name_wrapper}>
-            <chakra.FormControl id="firstName" style={{margin:"2px"}}>
-              <chakra.FormLabel>First Name</chakra.FormLabel>
-              <chakra.Input placeholder="First Name" />
-            </chakra.FormControl>
+    <form onSubmit={(e)=>{handleOnSubmit(e)}} className={style.form}>
+      <chakra.HStack className={style.HStack}>
+        <chakra.Flex
+          w="full"
+          h="full"
+          alignItems="center"
+          justifyContent="center"
+          borderRightWidth={1}
+          display={{ base: "none", md: "flex" }}
+        >
+          <chakra.Stack w="full" maxW="md" spacing={4} p={6}>
+            <chakra.Heading fontSize="2xl">Sign up</chakra.Heading>
+            <hr className={style.hr} />
+            <div className={style.all_name_wrapper}>
+              <chakra.FormControl id="firstName" style={{ margin: "2px" }}>
+                <chakra.FormLabel>First Name</chakra.FormLabel>
+                <chakra.Input placeholder="First Name" />
+              </chakra.FormControl>
 
-            <chakra.FormControl id="lastName" style={{margin:"2px"}}>
-              <chakra.FormLabel>Last Name</chakra.FormLabel>
-              <chakra.Input placeholder="Last Name" />
-            </chakra.FormControl>
-
-          </div>
+              <chakra.FormControl id="lastName" style={{ margin: "2px" }}>
+                <chakra.FormLabel>Last Name</chakra.FormLabel>
+                <chakra.Input placeholder="Last Name" />
+              </chakra.FormControl>
+            </div>
 
             <chakra.FormControl id="email">
               <chakra.FormLabel>Email</chakra.FormLabel>
               <chakra.Input placeholder="user@gmail.com" />
             </chakra.FormControl>
 
-          <chakra.FormControl id="password">
-            <chakra.FormLabel>Password</chakra.FormLabel>
-            <chakra.Input type="password" placeholder="******" />
-          </chakra.FormControl>
+            <chakra.FormControl id="passwordSignUp">
+              <chakra.FormLabel>Password</chakra.FormLabel>
+              <chakra.Input type="password" placeholder="******" />
+            </chakra.FormControl>
 
-          <chakra.FormControl id="passwordConfirm">
-            <chakra.FormLabel>Confirm your Password</chakra.FormLabel>
-            <chakra.Input type="password" placeholder="******" />
-          </chakra.FormControl>
+            <chakra.FormControl id="passwordConfirm">
+              <chakra.FormLabel>Confirm your Password</chakra.FormLabel>
+              <chakra.Input type="password" placeholder="******" />
+            </chakra.FormControl>
 
-          <chakra.Button>Create Account</chakra.Button>
-        </chakra.Stack>
-      </chakra.Flex>
-    </chakra.HStack>
+            <chakra.Button type="submit">Create Account</chakra.Button>
+          </chakra.Stack>
+        </chakra.Flex>
+      </chakra.HStack>
+    </form>
   );
 }
