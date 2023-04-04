2ef01ef8fe6f86f89348ce07a374ee659f615738
Colifaa <jorgemathez14@gmail.com>
2023-04-04 18:35:59 -0300

    modificated function getUserName

--- helpers/supabase_helpers/user_management.js
+++ helpers/supabase_helpers/user_management.js
@@ -171,8 +171,8 @@ export async function getUserName() {
       .select()
       .eq("id", uuid);
     if (error) throw error;
-    const { user_name } = data.at(0);
-    return user_name;
+    const { name } = data.at(0);
+    return name;
   } catch (error) {
     console.log(`Unable to retrieve name, there might be no user logged in.`);
     return undefined;
