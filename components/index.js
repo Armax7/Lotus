cec80b115823462a63bd69b0e8c105e4b1f581f6
Maxi Pogonza <pogonza00@gmail.com>
2023-04-05 14:51:52 -0300

    merge components/index from origin development

--- components/index.js
+++ components/index.js
@@ -1,5 +1,4 @@
 export { default as BackButton } from "./BackButton/BackButton";
-export { default as AddToCart } from "./AddToCart/AddToCart";
 export { default as RatingStars } from "./RatingStars/RatingStars";
 export { default as Carousel } from "./Carousel/Carousel";
 export { default as ArtworksInfo } from "./ArtworksInfo/ArtworksInfo";
@@ -11,4 +10,5 @@ export { default as CheckboxGroup } from "./CheckboxGroup/CheckboxGroup";
 export { default as SignIn } from "./login/signin/SignIn";
 export { default as SignUp } from "./login/signup/SignUp";
 export { default as SearchBar } from "./SearchBar/SearchBar";
-export { default as Loading} from "./Loading/Loading";
+export { default as Loading } from "./Loading/Loading";
+export { default as LogOutButton } from "./LogOutButton/LogOutButton";
