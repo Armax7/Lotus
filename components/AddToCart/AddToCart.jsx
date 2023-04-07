ba66b5486cbfe6c4d92446392bf72a0c28abeebf
Marina <95050756+Marindala@users.noreply.github.com>
2023-04-06 23:44:07 -0300

    Alert Component AddToCart

--- components/AddToCart/AddToCart.jsx
+++ components/AddToCart/AddToCart.jsx
@@ -1,9 +1,16 @@
 import { Button, Flex, Text } from "@chakra-ui/react";
 import { useState } from "react";
+import {
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+} from '@chakra-ui/react'
 
 function AddToCart({ stock, name, price, image, ...props }) {
   const [value, setValue] = useState(1);
   const [isMaxQuantity, setIsMaxQuantity] = useState(false);
+  const [isAddedToCart, setIsAddedToCart] = useState(false);
 
   const handleIncrement = () => {
     if (value < stock) {
@@ -18,6 +25,7 @@ function AddToCart({ stock, name, price, image, ...props }) {
       const newValue = value - 1;
       setValue(newValue);
       setIsMaxQuantity(false); // reset max quantity alert when decrementing
+      
     }
   };
 
@@ -39,61 +47,86 @@ function AddToCart({ stock, name, price, image, ...props }) {
       const existingCartItem = updatedCartItems[index];
       if (existingCartItem.quantity + value > stock) {
         setIsMaxQuantity(true);
+        
+      
+      
       } else {
         existingCartItem.quantity += value;
         localStorage.setItem("cartItems", JSON.stringify(updatedCartItems));
         setIsMaxQuantity(false);
+        setIsAddedToCart(true);
+        setTimeout(() => setIsAddedToCart(false), 2000);
+        
+        
       }
     } else {
       const newCartsItems = [...CartItems, newCartItem];
       localStorage.setItem("cartItems", JSON.stringify(newCartsItems));
       setIsMaxQuantity(false);
+      setIsAddedToCart(true);
+      setTimeout(() => setIsAddedToCart(false), 2000);
+    
+     
     }
   }
 
   return (
     <>
-      <Flex margin="50px 0 15px 0">
-        <Button size="sm" onClick={handleDecrement} disabled={value <= 1}>
-          -
-        </Button>
-        <Text mx={2} fontSize="md">
-          {value}
-        </Text>
-        <Button size="sm" onClick={handleIncrement} disabled={value >= stock}>
-          +
-        </Button>
-      </Flex>
-      {!isMaxQuantity ? (
-        <Button
-          backgroundColor="black"
-          color="white"
-          size="md"
-          css={{
-            "&:hover": {
-              transition: "all 0.5s ease-in-out",
-              color: "black",
-            },
-          }}
-          onClick={handleAddToCart}
-          disabled={isMaxQuantity}
-        >
-          Agregar al carrito
-        </Button>
-      ) : (
-        <Text
-          display="inline"
-          padding="10px"
-          borderRadius="2px"
-          marginTop="45px"
-          backgroundColor="red.500"
-          color="white"
-        >
+    <Flex margin="50px 0 15px 0">
+      <Button
+        size="sm"
+        onClick={handleDecrement}
+        disabled={value <= 1}
+      >
+        -
+      </Button>
+      <Text mx={2} fontSize="md">
+        {value}
+      </Text>
+      <Button
+        size="sm"
+        onClick={handleIncrement}
+        disabled={value >= stock}
+      >
+        +
+      </Button>
+    </Flex>
+    {isMaxQuantity ? (
+      <Alert status="error" marginBottom="10px">
+        <AlertIcon />
+        <AlertTitle mr={2}>UPS!:</AlertTitle>
+        <AlertDescription>
           Cantidad máxima alcanzada
-        </Text>
-      )}
-    </>
-  );
+        </AlertDescription>
+      </Alert>
+    ) : (
+      <Button
+        backgroundColor="black"
+        color="white"
+        size="md"
+        css={{
+          "&:hover": {
+            transition: "all 0.5s ease-in-out",
+            color: "black",
+          },
+        }}
+        onClick={handleAddToCart}
+        disabled={isMaxQuantity}
+      >
+        Agregar al carrito
+      </Button>
+    )}
+    {isAddedToCart && (
+      <Alert status="success" marginTop="10px">
+        <AlertIcon />
+        <AlertTitle mr={2}>Éxito:</AlertTitle>
+        <AlertDescription>
+          Agregado al carrito
+        </AlertDescription>
+      </Alert>
+    )}
+  </>
+);
 }
 
 export default AddToCart;
