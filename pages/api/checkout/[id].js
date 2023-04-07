54c58cb22891cc6f224a6909da9c460ead1dd681
Armando Isaac Garcia <armando7jx@gmail.com>
2023-04-07 13:11:54 -0600

    Corrected api/checkout/[id] handler

--- pages/api/checkout/[id].js
+++ pages/api/checkout/[id].js
@@ -1,5 +1,16 @@
 import * as Handlers from "../../../helpers/api_helpers/checkout_helpers/checkout_handlers";
+import * as Methods from "../../../helpers/api_helpers/methods";
 
-export default async function handlerCheckoutSessionId(req, res) {
-  return await Handlers.handleGetCheckoutSessionById(req, res);
+export default async function handlerCheckout(req, res) {
+  const method = req.method;
+
+  switch (method) {
+    case Methods.GET:
+      await Handlers.handleGetCheckoutSessionById(req, res);
+      break;
+    default:
+      return res.status(400).json({
+        message: "400 Bad Request: invalid method",
+      });
+  }
 }
