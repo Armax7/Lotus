e36381241037df46239fc077bf77b5a53b24e2a0
gekoJs <daniroa.js@gmail.com>
2023-04-10 11:55:07 -0400

    styling purchase success

--- pages/purchase/index.js
+++ pages/purchase/index.js
@@ -2,20 +2,54 @@ import * as Chakra from "@chakra-ui/react";
 
 function Purchase({ success }) {
   return (
-    <Chakra.Box>
+    <Chakra.Box
+      bg={"var(--color5)"}
+      height={"calc(100vh - 327px)"}
+      fontFamily={"Poppins"}
+      fontSize={"24px"}
+      padding={"40px"}
+    >
       {success === "true" ? (
-        <Chakra.Alert status="success">
-          <Chakra.AlertIcon />
-          <Chakra.AlertTitle>Thank you</Chakra.AlertTitle>
-          <Chakra.AlertDescription>
-            You purchase successful
+        <Chakra.Alert
+          status="success"
+          display={"flex"}
+          flexDir={"column"}
+          alignItems={"center"}
+          justifyContent={"center"}
+          maxW={"400px"}
+          w={"100%"}
+          maxH={"400px"}
+          h={"100%"}
+          borderRadius={"1000px"}
+          m={"auto"}
+          color={"var(--black)"}
+        >
+          <Chakra.AlertIcon m={"0 auto"} transform={"scale(2)"} mb={"10px"} />
+          <Chakra.AlertTitle m={"0 auto"} lineHeight={"50px"}>
+            Gracias
+          </Chakra.AlertTitle>
+          <Chakra.AlertDescription m={"0 auto"}>
+            Tu compra fue exitosa
           </Chakra.AlertDescription>
         </Chakra.Alert>
       ) : (
-        <Chakra.Alert status="warning">
-          <Chakra.AlertIcon />
-          <Chakra.AlertTitle>We are sorry</Chakra.AlertTitle>
-          <Chakra.AlertDescription>
+        <Chakra.Alert
+          status="warning"
+          display={"flex"}
+          flexDir={"column"}
+          alignItems={"center"}
+          justifyContent={"center"}
+          maxW={"400px"}
+          w={"100%"}
+          maxH={"400px"}
+          h={"100%"}
+          borderRadius={"1000px"}
+          m={"auto"}
+          color={"var(--black)"}
+        >
+          <Chakra.AlertIcon m={"0 auto"} transform={"scale(2)"} mb={"10px"}/>
+          <Chakra.AlertTitle  m={"0 auto"} lineHeight={"50px"}>We are sorry</Chakra.AlertTitle>
+          <Chakra.AlertDescription textAlign={"center"}>
             We could not complete your purchase
           </Chakra.AlertDescription>
         </Chakra.Alert>
