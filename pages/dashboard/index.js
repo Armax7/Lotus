963e36d782860ce786b4489e0f17354e525b2377
Marina <95050756+Marindala@users.noreply.github.com>
2023-04-12 16:18:31 -0300

    Dashboard

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -3,16 +3,12 @@ import React from "react";
 //import * as Components from "../../components";
 //import * as Chakra from "@chakra-ui/react";
 
-
-
 function Dashboard() {
-return(
-
-
-    <div> 
-           <h1> Hola Soy El Dashboard</h1>
+  return (
+    <div>
+      <h1> Hola Soy El Dashboard</h1>
     </div>
-)
+  );
 }
 
 export default Dashboard;
