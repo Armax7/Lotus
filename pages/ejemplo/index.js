cb32fc5b5d13ae5fd5afa64ae1e58752c89288e2
Armando Isaac Garcia <armando7jx@gmail.com>
2023-04-17 15:11:31 -0600

    Added option to use Disponible as logic delete

--- pages/ejemplo/index.js
+++ pages/ejemplo/index.js
@@ -1,15 +1,13 @@
 import * as Components from "../../components";
 import * as Chakra from "@chakra-ui/react";
 import * as Layouts from "../../layouts";
+import { cardMock } from "../../helpers/mocks/components_mocks/Cards_mock";
 
 function index() {
   return (
     <Chakra.Box>
       Art Details Supreme
-      <Components.BackButton href={"/"} />
-      <Components.SignUp />
-      <Components.SignIn />
-      <Components.LogOutButton />
+      <Components.UpdateArtworkForm artwork={cardMock} />
     </Chakra.Box>
   );
 }
