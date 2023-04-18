e7b11d972a675e04c95bf20f3741983e041da680
Marina <95050756+Marindala@users.noreply.github.com>
2023-04-18 00:04:47 -0300

    FE-98 Agregar seccion favoritos en Card con Alert/ en Navbar si está logueado se renderiza la opción favoritos

--- components/Card/Card.jsx
+++ components/Card/Card.jsx
@@ -1,9 +1,9 @@
 import * as Chakra from "@chakra-ui/react";
+import Swal from "sweetalert2";
 import Link from "next/link";
 import style from "./Card.module.css";
 import { largeTextHandler } from "../../helpers/utils";
 import { MdFavorite, MdFavoriteBorder } from "react-icons/md";
-
 
 function Card({
   artwork,
@@ -28,7 +28,11 @@ function Card({
   } = artwork;
 
   const handleLinkClick = () => {
-    alert("por favor loguerse")
+    Swal.fire({
+      text: "Necesitas iniciar sesión para agregar Obras a favoritos",
+      icon: "warning",
+      confirmButtonText: "OK",
+    });
   };
 
   return (
@@ -37,45 +41,45 @@ function Card({
       spacing={spacingProp}
       {...props}
     >
-      <Link href={`${baseHref}/${id}`}>
-        <Chakra.WrapItem>
-          <Chakra.Stack spacing="3">
-            <Chakra.Card
-              bgColor={"var(--colo5)"}
-              boxShadow={"none"}
-              maxW="280px"
-              minW="280px"
-              // minH={"max-content"}
-            >
-              <Chakra.CardBody className={style.card}>
+      <Chakra.WrapItem>
+        <Chakra.Stack spacing="3">
+          <Chakra.Card
+            bgColor={"var(--colo5)"}
+            boxShadow={"none"}
+            maxW="280px"
+            minW="280px"
+            // minH={"max-content"}
+          >
+            <Chakra.CardBody className={style.card}>
+              <Link href={`${baseHref}/${id}`}>
                 <img src={image} alt={name} className={style.img} />
-                <div>
-                  <Chakra.Heading
-                    size="md"
-                    fontFamily={"Poppins"}
-                    color={"var(--black)"}
-                  >
-                    {largeTextHandler(20, name)}
-                  </Chakra.Heading>
-                  <Chakra.Text color="var(--black)" fontSize="2xl">
-                    {`$${price}`}
-                    <span className={style.coin}>USD</span>
-                  </Chakra.Text>
-                  <Chakra.Flex justifyContent="end" rounded="md" px={1}>
-                    <Chakra.Icon
-                      as={MdFavoriteBorder}
-                      color="#FB8500"
-                      cursor="pointer"
-                      boxSize={23}
-                      onClick={handleLinkClick}
-                    />
-                  </Chakra.Flex>
-                </div>
-              </Chakra.CardBody>
-            </Chakra.Card>
-          </Chakra.Stack>
-        </Chakra.WrapItem>
-      </Link>
+              </Link>
+              <div>
+                <Chakra.Heading
+                  size="md"
+                  fontFamily={"Poppins"}
+                  color={"var(--black)"}
+                >
+                  {largeTextHandler(20, name)}
+                </Chakra.Heading>
+                <Chakra.Text color="var(--black)" fontSize="2xl">
+                  {`$${price}`}
+                  <span className={style.coin}>USD</span>
+                </Chakra.Text>
+                <Chakra.Flex justifyContent="end" rounded="md" px={1}>
+                  <Chakra.Icon
+                    as={MdFavoriteBorder}
+                    color="#80467491"
+                    cursor="pointer"
+                    boxSize={27}
+                    onClick={handleLinkClick}
+                  />
+                </Chakra.Flex>
+              </div>
+            </Chakra.CardBody>
+          </Chakra.Card>
+        </Chakra.Stack>
+      </Chakra.WrapItem>
     </Chakra.VStack>
   );
 }
