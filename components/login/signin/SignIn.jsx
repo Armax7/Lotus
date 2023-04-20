91a1be2dc8b2314956db4fe5de569f5c6f735347
Colifaa <jorgemathez14@gmail.com>
2023-04-20 12:23:30 -0300

    sin time out

--- components/login/signin/SignIn.jsx
+++ components/login/signin/SignIn.jsx
@@ -59,9 +59,9 @@ export default function SignIn({ onClose: onCloseProp = () => {}, ...props }) {
         const data = await UserAuth.userEmailLogIn(formData);
         console.log(data);
         setShowAlert(true);
-        /* setTimeout(() => {
+       
           location.reload();
-        }, 2000); */
+      
       } catch (error) {
         console.log(error);
       }
