bec2f2845ddf2aed02bc97fc3df950139531f375
Marina <95050756+Marindala@users.noreply.github.com>
2023-04-20 12:28:09 -0300

    Change favorite alert

--- components/Card/Card.jsx
+++ components/Card/Card.jsx
@@ -63,11 +63,11 @@ function Card({
       });
       setClick(true);
     } else {
-        Swal.fire({
+    /*     Swal.fire({
         text: "Puedes eliminarlo desde favoritos",
         icon: "warning",
         confirmButtonText: "OK",
-      }); 
+      }); */ 
       setIsAddedToCart(false);
       setClick(false);
     }
