ba5b68ca9b96553d9c2eb96ba05f821d507d5a36
Armando Isaac Garcia <armando7jx@gmail.com>
2023-04-24 18:20:25 -0600

    Documented utils

--- helpers/utils.js
+++ helpers/utils.js
@@ -1,4 +1,9 @@
-// The isObject function returns true if the value passed as an argument is an object and not an array, and false otherwise.
+/** The isObject function returns true if the value passed as an argument is an object and not an array, 
+ * and false otherwise.
+ * 
+ * @param {object} object Object to check
+ * @returns {boolean} True when received object is an object {}
+ */
 export function isObject(object) {
   return (
     !!object &&
@@ -7,7 +12,13 @@ export function isObject(object) {
     object !== null
   );
 }
-// The "sortDescCompareFn" function returns a comparator function that can be used with the sort method of an array to sort the elements in ascending order based on their values.
+
+/** The "sortDescCompareFn" function returns a comparator function that 
+ * can be used with the sort method of an array to sort the elements in ascending 
+ * order based on their values.
+ * 
+ * @returns {number} 1 when b>a, -1 when a>b and 0 when equal
+ */
 export function sortAscCompareFn() {
   return (a, b) => {
     if (a < b) {
@@ -18,7 +29,13 @@ export function sortAscCompareFn() {
     return 0;
   };
 }
-// The "sortDescCompareFn" function returns a comparator function that can be used with the sort method of an array to sort the elements in descending order based on their values.
+
+/** The "sortDescCompareFn" function returns a comparator function that 
+ * can be used with the sort method of an array to sort the elements in ascending 
+ * order based on their values.
+ * 
+ * @returns {number} 1 when b<a, -1 when a<b and 0 when equal
+ */
 export function sortDescCompareFn(a, b) {
   return (a, b) => {
     if (a > b) {
@@ -29,11 +46,22 @@ export function sortDescCompareFn(a, b) {
     return 0;
   };
 }
-// The "isSpace" function returns true if the given string only contains whitespace characters or is empty, and false otherwise.
+
+/** The "isSpace" function returns true if the given string only contains whitespace 
+ * characters or is empty, and false otherwise.
+ * 
+ * @param {string} str 
+ * @returns {boolean} True when string is empty or only whitespaces.
+ */
 export function isSpace(str) {
   return !str.replace(/\s/g, "").length;
 }
 
+/** Converts to string and trims object properties' values
+ * 
+ * @param {object} object 
+ * @returns 
+ */
 export function trimObjProperties(object) {
   if (!isObject(object)) throw new TypeError("Parameter should be an object");
 
